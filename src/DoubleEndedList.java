5e77ff5e8ee1a1cbc6a3b9d4ca9cefc580ba7550
saldivar <saldivar@saldivar-N55SF>
2017-01-19 22:09:17 -0600

    Double Link - not as expected

--- src/DoubleEndedList.java
+++ src/DoubleEndedList.java
@@ -41,7 +41,7 @@ public class DoubleEndedList {
 		}
 
 	}
-	
+
 	public void addLastPosition(String name) {
 
 		SNode newNode = new SNode(name);
@@ -56,57 +56,127 @@ public class DoubleEndedList {
 		}
 
 	}
-	
+
 	public void removeFirst() {
-		
+
+		if (!isEmpty()) {
+
+			if (first == last) {
+				first = null;
+				last = null;
+			} else {
+				first.next.previous = null;
+				first = first.next;
+			}
+
+		}
+
 	}
-	
+
 	public void removeLast() {
-		
+
+		if (!isEmpty()) {
+
+			if (first == last) {
+				first = null;
+				last = null;
+			} else {
+				last.previous.next = null;
+				last = last.previous;
+			}
+
+		}
+
 	}
-	
-	public void removeRecord() {
-		
+
+	public void removeRecord(String name) {
+
+		SNode previous = null;
+		SNode current = first;
+
+		while (!current.name.equalsIgnoreCase(name)) {
+
+			if (current.next == null) {
+				return;
+			}
+
+			previous = current;
+			current = current.next;
+
+		}
+
+		if (current == last) {
+
+			last = current.previous;
+			current.previous = previous;
+
+		} else if (current == first) {
+			
+			first = current.next;
+			current.next.previous = null;
+			
+		} else if (current.next != null) {
+
+			previous.next = current.next;
+			current.next.previous = previous;
+
+		} else {
+
+			last = current.previous;
+
+		}
+
 	}
-	
+
 	public void displayForwards() {
-		
-		SNode firstNode = first; 
-		
+
+		SNode firstNode = first;
+
 		while (firstNode != null) {
 			firstNode.display();
 			firstNode = firstNode.next;
 		}
-		
+
 	}
-	
-public void displayBackwards() {
-		
-		SNode lastNode = last; 
-		
+
+	public void displayBackwards() {
+
+		SNode lastNode = last;
+
 		while (lastNode != null) {
 			lastNode.display();
 			lastNode = lastNode.previous;
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
-		
-		DoubleEndedList doubleEndedList = new DoubleEndedList(); 
+
+		DoubleEndedList doubleEndedList = new DoubleEndedList();
 		doubleEndedList.addFirstPosition("D");
 		doubleEndedList.addFirstPosition("C");
 		doubleEndedList.addFirstPosition("B");
-		
+		doubleEndedList.addFirstPosition("B");
+
 		System.out.println("Display forward:");
 		doubleEndedList.displayForwards();
-	
+
 		System.out.println("\nDisplay backwards:");
 		doubleEndedList.displayBackwards();
-		
+
 		System.out.println("\nDisplay backwards again:");
 		doubleEndedList.addFirstPosition("A");
 		doubleEndedList.addLastPosition("Z");
+		doubleEndedList.displayBackwards();
+
+		System.out.println("\nRemove 2 last and 1 first");
+		doubleEndedList.removeLast();
+		doubleEndedList.removeLast();
+		doubleEndedList.removeFirst();
+		doubleEndedList.removeFirst();
+		doubleEndedList.removeRecord("C");
+		doubleEndedList.removeRecord("B");
+		doubleEndedList.addFirstPosition("A");
 		doubleEndedList.displayBackwards();
 	}
 
